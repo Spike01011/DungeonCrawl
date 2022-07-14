fe18c7a7c85439ccc9475df1ea8e8f390a2b506e
Marius_Cristian <cry.ichimas81@gmail.com>
2022-07-14 20:11:43 +0300

    jump work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,7 +58,7 @@ public class PlayerController : MyEntity
         baseAttackSpeed = 1.5f;
         baseSpeed = 500f;
         baseHp = 100;
-        jumpForce = 500f;
+        jumpForce = 20f;
 
         attackSpeedMult = 1.0f;
         damageMult = 1.0f;
@@ -157,15 +157,20 @@ public class PlayerController : MyEntity
         {
             anim.SetBool(isRunningHash, isRunning);
         }
-
+        int i = 0;
         if (isRunning)
         {
-            rb.velocity = transform.forward * Time.deltaTime * speed;
+            i = 1;
+            //rb.velocity = transform.forward * Time.deltaTime * speed;
         }
         else
-        {
-            rb.velocity = new Vector3(0, 0, 0);
+        {   i=0;
+            //rb.velocity = new Vector3(0, 0, 0);
+
         }
+        Vector3 velocity = ((transform.forward * i)) * speed * Time.fixedDeltaTime;
+        velocity.y = rb.velocity.y;
+        rb.velocity = velocity;
     }
 
     void Jump()
@@ -176,9 +181,7 @@ public class PlayerController : MyEntity
             //rb.velocity = transform.up * Time.deltaTime * jumpForce;
             rb.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
 
-            Vector3 velocity = ((transform.forward * speed)) * Time.fixedDeltaTime;
-            velocity.y = rb.velocity.y;
-            rb.velocity = velocity;
+            
         }
     }
    
